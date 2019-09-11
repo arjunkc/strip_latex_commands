1b004c169b2789a094d4e5005c03f636730bdb6f
arjunkc <arjunkc@gmail.com>
2019-09-10 21:41:41 -0400

    Small changes before making a big change: will solve the nesting problem with del_command

--- strip_latex_commands.py
+++ strip_latex_commands.py
@@ -10,6 +10,10 @@
 ## 1. Dec 06 2016 Found an error. It seems to be deleting args twice. The del_args function should really only delete one matched parenthesis; i.e., one argument.
 '''
 Notes
+Sep 10 2019 There are problems with del_commands
+    # \a{\b{}} then the del_locations will contain
+    # 0736 -> 0367 which will remove 0 to 3 and then 6 to 7
+We really have to store intervals in del_commands, and see remove those intervals that are subintervals of any other, and then delete those
 Mar 28 2018 Appear to have fixed the problems with del_commands. It now stores delete locations, and then deletes together at the end. Also uses finditer, which is a bit faster.
 Mar 27 2018 ~~Stuck on the code in del_commands which deletes a line. I'm assembling the final string from del locations.~~
 Mar 27 2018 It gives me trouble when I have an \opt line inside an equation, and this does not remove the statement if it's by itself on that line. This causes latex errors when compiling. This is easily fixed manually, but really, this should fix it.
@@ -20,9 +24,9 @@ Mar 27 2018 Fixed the following problem: if you find \arjun before \arjunhl, the
 import re
 import sys
 
-environs = ['note','longcalc','rambling','newnotes']
+environs = ['note','longcalc','rambling','newnotes','oldnotes']
 # list of [command, number of arguments]. For example `\opt{}{}` has 2 arguments. No spaces allowed between arguments.
-commands = [[r'\\opt',2],[r'\\arjun',1],[r'\\jon',1],[r'\\notes',1],[r'\\arjunhl',1],[r'\\arjunnew',1],[r'\\jeremy',1],[r'\\optlongcalc',1]]
+commands = [[r'\\opt',2],[r'\\arjun',1],[r'\\jon',1],[r'\\notes',1],[r'\\arjunhl',1],[r'\\arjunnew',1],[r'\\jeremy',1],[r'\\optlongcalc',1],[r'\\oldnotes',1],[r'\\arjunnotes',1]]
 debug = True
 interactive = False
 
@@ -72,17 +76,24 @@ def del_commands(s,commands):
         matchstring = c[0] + r'\s*(?={)'
         matches = re.finditer(matchstring,s)
 
+        if debug:
+            print("Removing command: ", c)
+
         for mat in matches:
             start = mat.start()
             end = mat.end()
 
             if debug:
-                print("Start and end of match: ",(start,end))
+                # prints start and end of command
+                print("Start and end of command: ",(start,end))
 
             # delete arguments of command. pass start pos of '{' to del_args; returns new end
             # del_args(full string,number of arguments, position to start searching for arguments)
             end = del_args(s,c[1],end)
-
+                
+            if debug:
+                # prints end of argument of command
+                print("Argument of command ends at: ",end)
 
             # is it of the form { \command{}{} }? then ignore
             prev_brace = re.search('^[^}]*{',s[0:start][::-1])
@@ -99,6 +110,7 @@ def del_commands(s,commands):
                     start = start - empty_space_before.end()
                 # delete the command too
 
+                # by default it will always del_line
                 del_line = 'y'
                 if interactive:
                     print('Command:\n', s[start:end])
@@ -107,7 +119,10 @@ def del_commands(s,commands):
                     del_locations = del_locations + [start,end]
                 
     del_locations.append(len(s)) #append length character to delete locations; it has even length
-    # potential source of error if nested commands
+    # potential source of error if nested commands; this is obvious if you have 
+    # \a{\b{}} then the del_locations will contain
+    # 0736 -> 0367 which will remove 0 to 3 and then 6 to 7
+    
     del_locations.sort()
 
     if debug:
